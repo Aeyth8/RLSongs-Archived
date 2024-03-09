a6366457f3b6443ef66a73c90d020558e09f482a
Aeyth <85146374+Aeyth8@users.noreply.github.com>
2024-03-08 19:07:34 -0500

    Variables

---
+++ Variables.py
@@ -0,0 +1,692 @@
+# Unreleased Tracks
+
+BattleBus = 1032070721
+
+BreathingUnderwater = 926502865
+
+DontStopTheParty = 922788769
+
+Firework = 660123685
+
+FireworkRemix = 1047270032
+
+Rave = 757233767
+
+SeeingWhatsNext = 993549143
+
+SeeingWhatsNextKF = 550760825
+
+SolarEclipses = 66944502
+
+SpellRemix = 880638321
+
+
+
+# Rocket League X Monstercat Vol. 1 
+
+O202 = 64670263
+
+AllMe = 848094805
+
+Apex = 88838090
+
+BoneDry = 400094985
+
+BreakMe = 267347516
+
+CallMe = 960539356
+
+Collisions = 16607948
+
+DriftAway = 44989323
+
+DropIt = 556822234
+
+Fury = 99542481
+
+LUVUNEEDU = 345422383
+
+Outbreak = 43329706
+
+PlayToWin = 64696673
+
+ROCKIT = 1008395929
+
+Skyforth = 142067588
+
+Twilight = 963228156
+
+WakeUpYoureDreaming = 309780099
+
+Weapon = 632682816
+
+
+
+# Rocket League X Monstercat Vol. 2
+
+Flow = 948145172
+
+MyLove = 242172996
+
+NewHorizons = 714941395
+
+PartyFreaks = 786811229
+
+TruthAndMalice = 586066515
+
+WhatAreYouWaitingFor = 714941395
+
+
+
+# Rocket League X Monstercat Vol. 3 
+
+AboutU = 677856927
+
+Glide = 936803680
+
+Hours = 233831254
+
+KeepYou = 131143909
+
+Shiawase = 613650024
+
+Silhouette = 963981264
+
+
+
+# Rocket League X Monstercat Vol. 4 
+
+BurningUp = 645263961
+
+Charge = 166384779
+
+Contra = 178285516
+
+FLY = 877916951
+
+Horsepower = 485986029
+
+WalkingOnTheMoon = 376057303
+
+
+
+# Rocket League X Monstercat Vol. 5 
+
+Badlands = 417651001
+
+Castaway = 700179548
+
+Expedition = 15423697
+
+FromHere = 290217386
+
+OhYou = 1035547060
+
+Runnin = 829768554
+
+
+
+# Rocket League X Monstercat Vol. 6 
+
+Amnesia = 81707114
+
+Color = 392821224
+
+EachOther = 575373577
+
+Hot = 575706100
+
+InternetBoy = 1018637404
+
+Play = 247811
+
+NoFearAnymore = 901460048
+
+US = 683426809
+
+the_darkness = 954488615
+
+Wishes = 901070040
+
+
+
+# Rocket League X Monstercat Vol. 7 
+
+Blinded = 652094770
+
+Bloom = 46312449
+
+Cobra = 359518551
+
+HeatWave = 394054889
+
+IllGo = 187100013
+
+MarkTwain = 776115329
+
+OnlyYou = 721348847
+
+Still = 605226612
+
+TestMe = 783186840
+
+TheOne = 186522556
+
+
+
+# Rocket League X Monstercat 2020 
+
+AllINeedS= 909839913
+
+BustItOut = 577145327
+
+ChasingClouds = 786739764
+
+Enigma = 31337015
+
+Fighters = 723897635
+
+Galaxies = 687340327
+
+Gold= 740171044
+
+IRemember = 461868208
+
+OdinsGhost = 928507950
+
+Revenger = 976709907
+
+Skin = 442195848
+
+SomeoneElse = 764778937
+
+Therapy = 620409834
+
+Voidwalkers = 313555232
+
+WorstPartOfHeaven = 638006948
+
+
+
+# Radical Music 
+
+AeonMetropolis = 430764306
+
+Carissa = 551682273
+
+DadsInSpace = 1016334278
+
+HangUp = 539881124
+
+IfICould = 568922042
+
+LetsTalk = 891584875
+
+Neon = 90378803
+
+PlayingWithTheBoys = 459895020
+
+Sundance = 178869697
+
+
+
+# Throttle - Where U Are [Deluxe] 
+
+ForMe = 777017283
+
+ForMeVIP = 599846011
+
+BloomVIP = 475984949
+
+Cities = 983345527
+
+CitiesAcoustic = 285168213
+
+Japan = 239296477
+
+JapanVIP = 978795169
+
+WhereUAre = 830325926
+
+WhereUAreVIP = 2863187
+
+
+
+# Monstercat X Beat Saber 
+
+EPIC = 906019181
+
+Boundless = 699048083
+
+FeelingStrongerHM = 672742999
+
+ThisTime = 664558359
+
+Rattlesnake = 764167670
+
+
+
+# Rocket League X Monstercat Legacy 
+
+FromHereRemix = 48644282
+
+HorsepowerRemix = 222397043
+
+OutbreakRemix = 720754059 
+
+ROCKITVIP = 832587522
+
+SilhouetteFMS = 891771221
+
+ShiawaseVIP = 645430770
+
+WalkingOnTheMoonRemix = 904316638
+
+WhatAreYouWaitingForVIP = 97849123
+
+
+
+# Kaskade X RL EP 
+
+Closer = 979687925
+
+FlipReset = 911773112
+
+MilesToGo = 777199614
+
+SolidGround = 731135284
+
+
+
+# Monstercat 10 Year Anniversary
+
+AllINeedS = 909839913
+
+Alone = 661072483
+
+AreWeStillYoung = 154975369
+
+CrabRave = 1060233894
+
+Disconnected = 923455551
+
+EverythingBlack = 717229935
+
+Nevada = 684951173
+
+Potions = 472324009
+
+Stronger = 1047332183
+
+WeWontBeAlone = 477538144
+
+
+
+# Monstercat Silk 
+
+Airborne = 974568283
+
+AllINeed = 775033323
+
+BackAgain = 1044874150
+
+DarkMagic = 575205560
+
+Dreamers = 647996295
+
+FeelsLikeHome = 783437320
+
+FindYou = 947790530
+
+Glow = 1063659060
+
+Horizon = 993900953
+
+TooFar = 419050326
+
+Nibbana = 25656817
+
+PortraitOfYou = 477907344
+
+Secrets = 54779233
+
+
+
+# HHM 2021 
+
+Loca = 296795090 
+
+SoGood = 647888527 
+
+WeOwnTheNight = 203896239
+
+
+
+# Sideswipe OST 
+
+ControlMe = 802268548
+
+Dreams = 663599560
+
+FadingWind = 941907959
+
+HaveFun = 302377528
+
+LightUp = 979230866
+
+Made4This = 421301391
+
+Mermaid = 56041757
+
+ThatLook = 96754293
+
+ThereAndBack = 989408186
+
+WaterResistant = 130933480
+
+
+
+# Rocket League X Monstercat 2021 
+
+Boogie = 191659808
+
+Change = 63844923
+
+Collapse = 223088003
+
+Dangerous = 554310906
+
+Endgame = 280106760
+
+EnterREM = 602216472
+
+Eternal = 609701636
+
+Good4Me = 319622667
+
+Inertia = 212191171
+
+KeepUpThePace = 1008396034
+
+MemoryBank = 661233188
+
+Moondance = 920320982
+
+MoveOn = 1006550454
+
+MyMind = 963993114
+
+NightRunners = 661047288
+
+SameMistake = 485835573
+
+Scared = 220462910
+
+Shadow = 833634803
+
+TheTaste = 595171280
+
+Union = 743494759
+
+VHS = 653113715
+
+Orbit = 898239923
+
+YourSpell = 139975016
+
+
+
+# Rocket League X Monstercat 2022 
+
+O1975 = 404981724
+
+O2hard2letgo = 890008211
+
+AllNight = 500960060
+
+AllWeNeeded = 4989071
+
+Angels = 828209790
+
+Arise = 587688769
+
+Cashmere = 228266115
+
+CINEMA = 663566278
+
+CINEMAVIP = 202820239
+
+CRY = 968232920
+
+DejaReeve = 700633853
+
+DoubleTap = 113370889
+
+Duality = 589618453
+
+FallingOff = 809996553
+
+FallingApart = 216591688
+
+Flying = 209331343
+
+GetToU = 742057430
+
+GoodTime = 902899790
+
+HitList = 77597109
+
+HOLDONME = 94102186
+
+justwannaluv = 224331962
+
+Lions = 638173543
+
+LostAndFound = 828226929
+
+Madness = 393308304
+
+Nirvana = 926976447
+
+OutOfTheDark = 586958111
+
+PowerStone = 364164810
+
+RightThrough = 26585258
+
+Sicko = 970286464
+
+SLEEPER = 614597702
+
+StainedGlass = 37344723
+
+TakeItAway = 947166265
+
+WayBack = 474189259
+
+whoweare = 810095507
+
+WontMakeASound = 1030871036
+
+WYGD = 527394699
+
+
+
+# Frosty Fest 2022 feat. Lofi Girl 
+
+FirstSnow  = 513889088
+
+FlyingSled  = 627229130
+
+Monday = 956229553
+
+WinterDrive = 779566148
+
+WinterMorning  = 741141139
+
+
+
+# Esports 
+
+AllWeNeeded = 4989071     ## Yes it's in here twice for some reason playing the same file though
+
+CODES = 13432383
+
+FALL = 713389105
+
+Hope = 703989821
+
+LetEmPray = 681519721
+
+Moment = 710736557
+
+MomentWF = 149359795
+
+Pandemonium = 190582968
+
+Saviour = 753198809
+
+
+
+# Rocket League X Monstercat 2023 
+
+AGONIZE = 886007212
+
+BackseatLovers = 247537294
+
+BeatItUp = 776566329
+
+BurbankNights = 754044080
+
+CollectingThoughts = 487134190
+
+DRIFTER = 475730598
+
+Droid = 445166319
+
+domyownthing = 247836294
+
+Electric = 499882566
+
+Fall = 140472107
+
+FAROUTVIP = 221375111
+
+FeelAgain = 904625241
+
+Fever = 248486004
+
+FightMe = 306182210
+
+FireInYourEyes = 239826647
+
+FoolMe = 812508254
+
+GETINTHECAR = 642004028
+
+GetThrough = 236509588
+
+Golden = 1010419970
+
+Groovemasta = 138920434
+
+HighEnough = 260747555
+
+HitMyLine = 49647108
+
+Ignite = 431639282
+
+InLovingMemories = 439552872
+
+JustLikeYou = 13202239
+
+Kibo = 451013991
+
+King = 469432344 ## MOST ANNOYING GODDAMN SONG I'VE EVER HEARD IN ROCKET LEAGUE
+
+LionsVIP = 772810045
+
+magic = 496701760
+
+Machine = 961828371
+
+Mirror = 757801103
+
+Mothership = 899411316
+
+Motor = 788224244
+
+NaoSeiParar = 172221152
+
+NeverLetYouDown = 712567237
+
+oasis = 699020747
+
+OverTheWater = 780903673
+
+RIOT = 873881337
+
+Roots = 261129404
+
+SaferPlace = 699738523
+
+SameThing = 379976426
+
+Sapphire = 359745139
+
+SetYouFree = 515025403
+
+Stay = 853773918
+
+STEREO = 1003894207
+
+Strangers = 132854017
+
+TakeMeIntoSpace = 528513529
+
+Tension = 177538177
+
+TheLaw = 909771193
+
+TheOutlines = 26025501
+
+uwantme = 547228594
+
+UsedToTheSilence = 808799176
+
+Valhalla = 90645555
+
+WeAreAllEqual = 929487056
+
+WorkMyBody = 945734058 
+
+
+
+#Rocket League X Monstercat 2024
+
+O4D = 902221093
+
+AllIWant = 504339118
+
+BLEEPBLOOP = 139844126
+
+BOUNCE = 619787968
+
+Chromatically = 247811
+
+DigitalFriends = 648855622
+
+Ghost = 359555604
+
+NoTomorrow = 931824403
+
+Pump = 1019924526
+
+LeaveTheWorldBehind = 426029182
+
+
+
+
+
+
+
